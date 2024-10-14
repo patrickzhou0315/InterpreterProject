39f004b25bbff38d84ad2a6bba356bc43d692de0
Patrick Zhou <patrickxc.zhou@gmail.com>
2024-10-13 18:38:37 -0700

    a lot of issues with the code overall, so reformatted with the given pseudocode and handled the assignment expression stuff better

--- interpreterv1.py
+++ interpreterv1.py
@@ -3,11 +3,11 @@ from brewparse import parse_program
 
 
 class Interpreter(InterpreterBase):
-    variable_name_to_value = {}
-    variable_names = []
 
     def __init__(self, console_output=True, inp=None, trace_output=False):
         super().__init__(console_output, inp)   # call InterpreterBase's constructor
+        self.variable_name_to_value = {}
+        self.variable_names = []
 
     def run(self, program):
         # program is array of strings we want to interpret
@@ -16,8 +16,8 @@ class Interpreter(InterpreterBase):
         self.run_func(main_func_node)
 
     def get_main_func_node(self, ast):
-        for node in ast:
-            if node.elem_type == 'program':
+        for node in ast.dict['functions']:
+            if node.dict['name'] == 'main':
                 return node
         super().error(
             ErrorType.NAME_ERROR,
@@ -30,77 +30,119 @@ class Interpreter(InterpreterBase):
         
     def run_statement(self, statement_node):
         if statement_node.elem_type == 'vardef':
-            if statement_node.dict['name'] in self.variable_names:
-                super().error(
-                    ErrorType.NAME_ERROR,
-                    f"Variable {statement_node.dict['name']} defined more than once",
-                )
-            self.variable_names.append(statement_node.dict['name'])
+            self.variable_definition(statement_node)
         elif statement_node.elem_type == '=':
             self.do_assignment(statement_node)
         elif statement_node.elem_type == 'fcall':
             self.function_call(statement_node)
     
+    def variable_definition(self, statement_node):
+        if statement_node.dict['name'] in self.variable_names:
+            super().error(
+                    ErrorType.NAME_ERROR,
+                    f"Variable {statement_node.dict['name']} defined more than once",
+                    )
+        self.variable_names.append(statement_node.dict['name'])
+        self.variable_name_to_value[statement_node.dict['name']] = None   
+
     def do_assignment(self, assignment_node):
         if assignment_node.dict['name'] not in self.variable_names:
                 super().error(
                     ErrorType.NAME_ERROR,
-                    f"Variable {assignment_node.dict['expression'].dict['name']} not defined yet",
+                    f"Variable {assignment_node.dict['name']} not defined yet",
                     )
-        if assignment_node.dict['expression'].elem_type == 'var':
-            self.variable_name_to_value[assignment_node.dict['name']] = self.variable_name_to_value[assignment_node.dict['expression'].dict['name']]
-        elif assignment_node.dict['expression'].elem_type == 'int':
-            self.variable_name_to_value[assignment_node.dict['name']] = (int)(assignment_node.dict['expression'].dict['val'])
-        elif assignment_node.dict['expression'].elem_type == 'string':
-            self.variable_name_to_value[assignment_node.dict['name']] = assignment_node.dict['expression'].dict['val']
-        elif assignment_node.dict['expression'].elem_type == '+':
-            self.variable_name_to_value[assignment_node.dict['name']] = self.handle_expression(assignment_node.dict['expression'].dict['op1'], assignment_node.dict['expression'].dict['op2'], '+')
-        elif assignment_node.dict['expression'].elem_type == '-':
-            self.variable_name_to_value[assignment_node.dict['name']] = self.handle_expression(assignment_node.dict['expression'].dict['op1'], assignment_node.dict['expression'].dict['op2'], '-')
-            
+        self.variable_name_to_value[assignment_node.dict['name']] = self.evaluate_expression(assignment_node.dict['expression'])
+
+    def evaluate_expression(self, expression_node):
+        if expression_node.elem_type == 'var':
+            if expression_node.dict['name'] not in self.variable_names:
+                super().error(
+                    ErrorType.NAME_ERROR,
+                    f"variable {expression_node.dict['name']} undefined"
+                )
+            return self.variable_name_to_value[expression_node.dict['name']]
+        elif expression_node.elem_type == 'int':
+            return int(expression_node.dict['val'])
+        elif expression_node.elem_type == 'string':
+            return expression_node.dict['val']
+        elif expression_node.elem_type == '+' or expression_node.elem_type == '-':
+            return self.handle_expression(expression_node.dict['op1'], expression_node.dict['op2'], expression_node.dict['name'])
+        elif expression_node.elem_type == 'fcall':
+            if 'inputi' != expression_node.dict['name']:
+                super().error(
+                    ErrorType.NAME_ERROR,
+                    f"Not an input"
+                )
+            return InterpreterBase.get_input()
+
     def handle_expression(self, op1, op2, operation):
-        if op1.elem_type == '+' or op1.elem_type == '-':
+
+        # if the expression has expressions in it
+        # if both sides are expressions
+        if op1.elem_type in ['+', '-'] and op2.elem_type in ['+', '-']:
             if operation == '+':
-                return self.handle_expression(op1.op1, op1.op2, op1.elem_type) + op2
-            elif operation == '-':
-                return self.handle_expression(op1.op1, op1.op2, op1.elem_type) - op2
-        elif op2.elem_type == '+' or op2.elem_type == '-':
+                return self.handle_expression(op1.dict['op1'], op1.dict['op2'], op1.elem_type) + self.handle_expression(op2.dict['op1'], op2.dict['op2'], op2.elem_type)
+            return self.handle_expression(op1.dict['op1'], op1.dict['op2'], op1.elem_type) - self.handle_expression(op2.dict['op1'], op2.dict['op2'], op2.elem_type)
+        # if the expression is only on left side
+        elif op1.elem_type in ['+', '-']:
+            # recursively call expression thing on the expression
             if operation == '+':
-                return op1 + self.handle_expression(op2.op1, op2.op2, op2.elem_type)
+                return self.handle_expression(op1.dict['op1'], op1.dict['op2'], op1.elem_type) + self.evaluate_expression(op2)
             elif operation == '-':
-                return op1 - self.handle_expression(op2.op1, op2.op2, op2.elem_type)
+                return self.handle_expression(op1.dict['op1'], op1.dict['op2'], op1.elem_type) - self.evaluate_expression(op2)
+        # if the expression is only on the right side
+        elif op2.elem_type in ['+', '-']:
+            if operation == '+':
+                return self.evaluate_expression(op1) + self.handle_expression(op2.dict['op1'], op2.dict['op2'], op2.elem_type)
+            elif operation == '-':
+                return self.evaluate_expression(op2) - self.handle_expression(op2.dict['op1'], op2.dict['op2'], op2.elem_type)
+        # if any of the operators are strings
         elif op1.elem_type == 'string' or op2.elem_type == 'string':
             super().error(
                     ErrorType.NAME_ERROR,
-                    f"Unsupported operation (string)",
+                    f"Unsupported operation (string concatenation when defining variables)",
                 )
-        elif op1.elem_type == 'int' or op2.elem_type == 'int':
-            if op1.elem_type == 'int':
-                return self.handle_expression((int)(op1.dict['val']), op2, operation)
-            return self.handle_expression(op1, (int)(op2.dict['val'], operation))
-        elif op1.elem_type == 'var' or op2.elem_type == 'var':
-            if op1.dict['name'] not in self.variable_names or op2.dict['name'] not in self.variable_names:
+        # if any of the operators are variables
+        # checked if any were strings or expressions already, operations can either be Value (ints) or variables
+        
+        elif op1.elem_type == 'var':
+            temp_var = self.evaluate_expression(op1)
+            if not isinstance(temp_var, int):
                     super().error(
                     ErrorType.NAME_ERROR,
-                    f"variable doesn't exist",
+                    f"Unsupported operation (string concatenation when defining variables)",
                 )
-            if op1.dict['name'].isdigit() == False or op2.dict['name'].isdigit() == False:
-                super().error(
-                    ErrorType.NAME_ERROR,
-                    f"variable is of type string",
-                )
-            if op1.elem_type == 'var':
-                return self.handle_expression(self.variable_name_to_value[op1.dict['name']], op2, operation)
-            return self.handle_expression(op1, self.variable_name_to_value[op2.dict['name']], operation)
-        elif type(op1) == int and type(op2) == int:
             if operation == '+':
-                return op1 + op2
-            return op1 - op2
+                return temp_var + self.evaluate_expression(op2)
+            return temp_var - self.evaluate_expression(op2)
+        
+        elif op2.elem_type == 'var':
+            temp_var = self.evaluate_expression(op2)
+            if not isinstance(temp_var, int):
+                    super().error(
+                    ErrorType.NAME_ERROR,
+                    f"Unsupported operation (string concatenation when defining variables)",
+                )
+            if operation == '+':
+                return temp_var + self.evaluate_expression(op1)
+            return self.evaluate_expression(op1) - temp_var
+        
+        # if both operators are ints
+        elif op1.elem_type == 'int':
+            if operation == '+':
+                return int(op1.dict['val']) + self.evaluate_expression(op2)
+            return int(op1.dict['val']) - self.evaluate_expression(op2)
+        
+        elif op2.elem_type == 'int':
+            if operation == '+':
+                return int(op2.dict['val']) + self.evaluate_expression(op1)
+            return self.evaluate_expression(op1) - int(op2.dict['val'])
         else:
             super().error(
                     ErrorType.NAME_ERROR,
                     f"Unsupported expression",
                 )
+            
     def function_call(self, function_node):
         if function_node.dict['name'] == 'print':
             self.handle_print(function_node.dict['args'])
@@ -113,15 +155,8 @@ class Interpreter(InterpreterBase):
                 )
             
     def handle_print(self, argument_nodes):
+        output = ""
         for argument in argument_nodes:
-            if argument.elem_type == '+' or argument.elem_type == '-':
-                InterpreterBase.output(argument.op1, argument.op2, argument.elem_type)
-            elif argument.elem_type == 'var':
-                InterpreterBase.output(self.variable_name_to_value[argument.dict['name']])
-            elif argument.elem_type == 'int' or argument.elem_type == 'string':
-                InterpreterBase.output(argument.dict['val'])
-            else:
-                super().error(
-                    ErrorType.NAME_ERROR,
-                    f"Cannot print the arguments",
-                )
+            output += str(self.evaluate_expression(argument))
+        InterpreterBase.output(output)
+    
